8de5fbd80d475c8f43847f039b8112bb0854f341
FelixTanaka <220711692@students.uajy.ac.id>
2024-11-16 16:37:03 +0700

    first commit

--- Tugas_Modul5_B_Felix_11692.py
+++ Tugas_Modul5_B_Felix_11692.py
@@ -4,8 +4,7 @@ import pickle
 from PIL import Image
 import os
 
-model_directory = r'C:\Users\LENOVO\Downloads\Introduction to Deep Learning (Praktek)\Introduction to Deep Learning (Praktek)'
-model_path = os.path.join(model_directory, r'best_model.pkl')
+model_path = r'best_model.pkl'
 
 if os.path.exists(model_path):
     try:
